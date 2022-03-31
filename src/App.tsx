a02a1e410624113085f6789076ca25403c4c932d
justincueva02 <justincueva02@gmail.com>
2022-03-31 15:26:11 -0700

    installed and setup react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import QuoteContainer from "./components/QuoteContainer";
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <QuoteContainer />
+      <BrowserRouter>
+        <Routes>
+          <Route path={"/"} element={<QuoteContainer />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
