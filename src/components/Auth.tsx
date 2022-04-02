4aab493eb47b0dd237e3125e880de033c1d8d0db
justincueva02 <justincueva02@gmail.com>
2022-04-02 10:39:32 -0700

    making form respond to theme

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
+
 import "../styles/Auth.css";
+import { useTheme, Theme } from "../hooks/Theme";
 
 const Auth = () => {
+  const { theme } = useTheme();
   const [formState, setFormState] = useState<string>("LOGIN");
 
   const loginHandler = (e: React.FormEvent<HTMLFormElement>) => {
@@ -14,30 +17,40 @@ const Auth = () => {
     console.log("creating account .....");
   };
 
+  const bg = theme === Theme.Light ? "bg-grey-100" : "bg-grey-900";
+  const col = theme === Theme.Light ? "col-grey-900" : "col-grey-100";
+
   return (
     <div className="container">
-      <div className="auth-container">
+      <div className={`auth-container ${bg}`}>
         <form
           className="form--auth"
           onSubmit={(e) => {
             formState === "LOGIN" ? loginHandler(e) : createAccountHandler(e);
           }}
         >
-          <h2 className="auth__heading">
+          <h2 className={`auth__heading ${col}`}>
             {formState === "LOGIN" ? "Login" : "Sign Up"}
           </h2>
           <div className="auth__fields">
             <div className="auth__field">
-              <label htmlFor="email">Your Email</label>
+              <label className={col} htmlFor="email">
+                Your Email
+              </label>
               <input autoComplete="off" name="email" type={"email"} />
             </div>
             <div className="auth__field">
-              <label htmlFor="password">Your Password</label>
+              <label className={col} htmlFor="password">
+                Your Password
+              </label>
               <input autoComplete="off" name="password" type={"password"} />
             </div>
           </div>
           <div className="auth__buttons">
-            <button type="submit" className="btn--submit">
+            <button
+              type="submit"
+              className="btn--submit bg-grey-medium col-grey-900"
+            >
               {formState === "LOGIN" ? "Login" : "Create Account"}
             </button>
             <button
@@ -47,7 +60,7 @@ const Auth = () => {
                 });
               }}
               type="button"
-              className="btn--change-form-state col-grey-900"
+              className={`btn--change-form-state ${col}`}
             >
               {formState === "LOGIN"
                 ? "Create new account"
